af3326fad9b52ddac3530e2bd37e05e79f4c9b37
FongMi <b8892778@gmail.com>
2023-01-03 16:44:25 +0800

    Update ali

--- app/src/main/java/com/github/catvod/spider/PushAgent.java
+++ app/src/main/java/com/github/catvod/spider/PushAgent.java
@@ -15,7 +15,7 @@ public class PushAgent extends Spider {
 
     @Override
     public void init(Context context, String extend) {
-        ali = new Ali(extend);
+        ali = Init.getAli().token(extend);
     }
 
     @Override
