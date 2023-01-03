ed60fdfa0f1842408cbc0a4e6b05007f945a564f
FongMi <b8892778@gmail.com>
2023-01-04 00:19:08 +0800

    Update Ali

--- app/src/main/java/com/github/catvod/spider/PushAgent.java
+++ app/src/main/java/com/github/catvod/spider/PushAgent.java
@@ -9,29 +9,38 @@ import com.github.catvod.utils.Misc;
 
 import java.util.List;
 
-public class PushAgent extends Spider {
-
-    private Ali ali;
+public class Push extends Spider {
 
     @Override
     public void init(Context context, String extend) {
-        ali = Init.getAli().token(extend);
+        Ali.get().init(extend);
     }
 
     @Override
     public String detailContent(List<String> ids) throws Exception {
         String url = ids.get(0).trim();
-        if (url.contains("aliyundrive")) return ali.detailContent(ids);
-        if (Misc.isVip(url)) return Result.string(ali.vod(url, "官源"));
-        if (Misc.isVideoFormat(url)) return Result.string(ali.vod(url, "直连"));
-        return Result.string(ali.vod(url, "网页"));
+        if (url.contains("aliyundrive")) return Ali.get().detailContent(ids);
+        if (Misc.isVip(url)) return Result.string(vod(url, "官源"));
+        if (Misc.isVideoFormat(url)) return Result.string(vod(url, "直連"));
+        return Result.string(vod(url, "網頁"));
     }
 
     @Override
     public String playerContent(String flag, String id, List<String> vipFlags) {
-        if (flag.contains("画")) return ali.playerContent(flag, id);
+        if (flag.contains("畫")) return Ali.get().playerContent(flag, id);
         if (flag.equals("官源")) return Result.get().parse().jx().url(id).string();
-        if (flag.equals("网页")) return Result.get().parse().url(id).string();
+        if (flag.equals("網頁")) return Result.get().parse().url(id).string();
         return Result.get().url(id).string();
+    }
+
+    private Vod vod(String url, String type) {
+        Vod vod = new Vod();
+        vod.setTypeName(type);
+        vod.setVodId(url);
+        vod.setVodName(url);
+        vod.setVodPlayFrom(type);
+        vod.setVodPlayUrl("播放$" + url);
+        vod.setVodPic("https://pic.rmb.bdstatic.com/bjh/1d0b02d0f57f0a42201f92caba5107ed.jpeg");
+        return vod;
     }
 }
