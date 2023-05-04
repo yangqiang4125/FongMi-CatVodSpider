c51e8cd59805bf416b37f1860c4629da4367fc23
yangqiang3504 <412594121@qq.com>
2023-05-04 10:58:39 +0800

    fix-- PanSearch

--- app/src/main/java/com/github/catvod/spider/Zhaozy.java
+++ app/src/main/java/com/github/catvod/spider/Zhaozy.java
@@ -9,7 +9,6 @@ import com.github.catvod.utils.Utils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 import java.net.URLEncoder;
 import java.util.ArrayList;
@@ -19,15 +18,13 @@ import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-
 public class Zhaozy extends Ali {
-    private static String b = "https://zhaoziyuan.la/";
-    private Pattern regexVid = Pattern.compile("(\\S+)");
-
+    private final Pattern regexVid = Pattern.compile("(\\S+)");
+    private final String siteUrl = "https://zhaoziyuan.la/";
     private Map<String, String> getHeader() {
         Map<String, String> headers = new HashMap<>();
         headers.put("User-Agent", Utils.CHROME);
-        headers.put("Referer", b);
+        headers.put("Referer", siteUrl);
         headers.put("Cookie", getCookie());
         return headers;
     }
@@ -39,10 +36,10 @@ public class Zhaozy extends Ali {
             params.put("password", "qq@4125");
             Map<String, String> headers = new HashMap<>();
             headers.put("User-Agent", Utils.CHROME);
-            headers.put("Referer", b + "login.html");
-            headers.put("Origin", b);
+            headers.put("Referer", siteUrl + "login.html");
+            headers.put("Origin", siteUrl);
             Map<String, List<String>> resp = new HashMap<>();
-            OkHttp.post(b + "logiu.html", params, headers, resp);
+            OkHttp.post(siteUrl + "logiu.html", params, headers, resp);
             StringBuilder sb = new StringBuilder();
             for (String item : resp.get("set-cookie")) sb.append(item.split(";")[0]).append(";");
             Utils.zzy = sb.toString();
@@ -51,7 +48,7 @@ public class Zhaozy extends Ali {
     }
 
     @Override
-    public String detailContent(List<String> list) {
+    public String detailContent(List<String> list) throws Exception {
         try {
             String id =list.get(0);
             if (!id.contains("aliyundrive.com")) {
@@ -72,38 +69,26 @@ public class Zhaozy extends Ali {
     }
 
     @Override
-    public String searchContent(String key, boolean quick) {
-        try {
-            String url = b + "so?filename=" + URLEncoder.encode(key);
-            Document docs = Jsoup.parse(OkHttp.string(url, getHeader()));
-            Elements list = docs.select("div.li_con div.news_text");
-            String pic = "https://inews.gtimg.com/newsapp_bt/0/13263837859/1000";
-            List<Vod> items = new ArrayList<>();
-            for (int i = 0; i < list.size(); i++) {
-                Element doc = list.get(i);
-                String title = doc.select("div.news_text a h3").text();
-                if (title.contains(key)) {
-                    String list1 = doc.select("div.news_text a").attr("href");
-                    Matcher matcher = regexVid.matcher(list1);
-                    if (matcher.find()) {
-                        String id = b + matcher.group(1);
-                        String remark = doc.select("div.news_text a p").text();
-                        //类别：文件夹 | 收录时间：2022-10-08 22:51
-                        remark = remark.replaceAll(".*收录时间：(.*)", "$1");
-                        Vod vod = new Vod();
-                        vod.setVodId(id + "$$$" + pic + "$$$" + title);
-                        vod.setVodName(title);
-                        vod.setVodPic(pic);
-                        vod.setVodRemarks(remark);
-                        items.add(vod);
-                    }
-                }
-            }
-            return Result.string(items);
-        } catch (Exception e) {
-            Utils.zzy = null;
-            SpiderDebug.log(e);
+    public String searchContent(String key, boolean quick) throws Exception {
+        String url = siteUrl + "sos?filename=" + URLEncoder.encode(key);
+        Document doc = Jsoup.parse(OkHttp.string(url, getHeader()));
+        List<Vod> list = new ArrayList<>();
+        String pic = "https://inews.gtimg.com/newsapp_bt/0/13263837859/1000";
+        for (Element element : doc.select("div.li_con div.news_text")) {
+            String href = element.select("div.news_text a").attr("href");
+            Matcher matcher = regexVid.matcher(href);
+            if (!matcher.find()) continue;
+            String id = siteUrl + matcher.group(1);
+            String name = element.select("div.news_text a h3").text();
+            if (!name.contains(key)) continue;
+            String remark = element.select("div.news_text a p").text().split("\\|")[1].split("：")[1];
+            Vod vod = new Vod();
+            vod.setVodPic(pic);
+            vod.setVodId(id + "$$$" + pic + "$$$" + name);
+            vod.setVodRemarks(remark);
+            vod.setVodName(name);
+            list.add(vod);
         }
-        return "";
+        return Result.string(list);
     }
 }
