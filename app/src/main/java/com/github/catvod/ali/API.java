7559fb9fd02fe5eebcf879fbd5d235edb92855b0
yangqiang3504 <412594121@qq.com>
2023-05-31 14:32:37 +0800

    fix-- douban 1133

--- app/src/main/java/com/github/catvod/ali/API.java
+++ app/src/main/java/com/github/catvod/ali/API.java
@@ -250,7 +250,8 @@ public class API {
             List<Item> files = new ArrayList<>();
             LinkedHashMap<String, List<String>> subMap = new LinkedHashMap<>();
             listFiles(new Item(getParentFileId(fileId, object)), files, subMap);
-            for (Item file : files) playUrls.add(Trans.get(file.getDisplayName()) + "$" + file.getFileId() + findSubs(file.getName(), subMap));
+            if(files.isEmpty())Init.show("来晚啦，该分享已失效");
+            else for (Item file : files) playUrls.add(Trans.get(file.getDisplayName()) + "$" + file.getFileId() + findSubs(file.getName(), subMap));
         } catch (Exception e) {
         }
         boolean fp = playUrls.isEmpty();
