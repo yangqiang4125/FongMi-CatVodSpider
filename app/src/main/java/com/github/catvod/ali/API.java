f549dabe8648970e88067ed7bec18ea7f06ec7d1
yangqiang3504 <412594121@qq.com>
2023-09-26 16:37:39 +0800

    fix-- parentDir6

--- app/src/main/java/com/github/catvod/ali/API.java
+++ app/src/main/java/com/github/catvod/ali/API.java
@@ -82,9 +82,8 @@ public class API {
         setAuth(true);
     }
     public void alert(String msg) {
-      /*  boolean aflag = Prefers.getBoolean("alert", false);
-        if(aflag) Init.show(msg);*/
-        Init.show(msg);
+        boolean aflag = Prefers.getBoolean("alert", false);
+        if(aflag) Init.show(msg);
     }
     public void setRefreshToken(String token) {
         if (auth.getRefreshToken().isEmpty()&&!token.endsWith(".json")) auth.setRefreshToken(token);
