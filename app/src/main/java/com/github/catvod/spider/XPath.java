dafad806c4b95bacbacb8814e7b84854f906d08c
yangqiang4125 <412594121@qq.com>
2023-10-08 16:44:08 +0800

    Update XPath.java

--- app/src/main/java/com/github/catvod/spider/XPath.java
+++ app/src/main/java/com/github/catvod/spider/XPath.java
@@ -32,13 +32,7 @@ public class XPath extends Spider {
         return headers;
     }
 
-    @Override
-    public void init(Context context) {
-        super.init(context);
-    }
-
     public void init(Context context, String extend) {
-        super.init(context, extend);
         this.ext = extend;
     }
 
