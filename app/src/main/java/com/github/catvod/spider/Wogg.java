c9f22a906ab8650ba6406f65ff5505291869c618
yangqiang4125 <412594121@qq.com>
2024-01-16 17:21:47 +0800

    Update Wogg.java

--- app/src/main/java/com/github/catvod/spider/Wogg.java
+++ app/src/main/java/com/github/catvod/spider/Wogg.java
@@ -23,9 +23,7 @@ public class Wogg extends Ali {
     @Override
     public void init(Context context, String extend) {
         String [] arr = inits(context, extend,"http://tvfan.xxooo.cf");
-        String extJson = "";
-        if(arr.length>2&&arr[2].endsWith(".json")) extJson = arr[2];
-        if(extJson.isEmpty())extJson ="{\n" +
+        String extJson ="{" +
                 "  \"ua\":1,\n" +
                 "  \"name\": \"Wogg\",\n" +
                 "  \"siteUrl\": \"https://tvfan.xxooo.cf\", \n" +
