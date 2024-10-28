d89fac7460cd155128773effab981652d2a0262d
yangqiang4125 <412594121@qq.com>
2024-10-28 16:44:39 +0800

    Update MyQQ.java

--- app/src/main/java/com/github/catvod/spider/MyQQ.java
+++ app/src/main/java/com/github/catvod/spider/MyQQ.java
@@ -370,14 +370,13 @@ public class MyQQ extends Spider {
                         String k = m.group(1);
                         if (k.contains("演员") || k.contains("主演")) vod.setVodActor(value);
                         else if (k.contains("导演")) vod.setVodDirector(value);
-                        else if (k.contains("状态")) vod.setVodRemarks(value);
                         else if (k.contains("首映")||k.contains("上映")) vod.setVodYear(value);
-                        else if (k.contains("集")||k.contains("备注")||k.contains("更新")) {
-                            if(Utils.matcher(".*\\d+集.*",value).matches()||Utils.matcher("\\d+",value).matches()||value.contains("全集")||value.contains("完结")) vod.setVodTag(value);
+                        else if (k.contains("集")||k.contains("备注")||k.contains("更新")||k.contains("状态")) {
+                            if(Utils.matcher(".*\\d+集.*",value).matches()||value.contains("全集")||value.contains("完结")) vod.setVodTag(value);
                             else if (val.contains("集")) {
                                 value = val.replaceAll(".*?(\\d+)集.*", "$1集");
                                 vod.setVodTag(value);
-                            } else value = "";
+                            }
                         } else if (k.contains("简介") || k.contains("介绍") || k.contains("详情")|| k.contains("剧情")) vod.setVodContent(value);
                     }
                 }
